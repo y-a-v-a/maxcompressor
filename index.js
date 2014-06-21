6fb54b307bf78208d6010e78c22f89040ccf837a
Vincent Bruijn <vebruijn@gmail.com>
2014-06-21 22:08:02 +0200

    added usage of GD

---
+++ index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var gd = require('node-gd');
+
+var image = './image.jpg';
+
+gd.openJpeg(image, function(err, img) {
+    if (err) {
+        throw err;
+    }
+    
+    return img.saveJpeg('./image1.jpg', 0, function(err) {
+        if (err) {
+            throw err;
+        }
+        return console.log('saved!');
+    });
+});
+
+console.log(a);
